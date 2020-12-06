d4e1e7f509ceba2f4b08b96705e363c98cbbbebc
Pavel <tr@tr.tr>
2020-12-06 11:20:59 +0300

    i

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {UrlTile} from 'react-native-maps';
+
+
+
 
 export default function App() {
+
+  const [region, setRegion] = useState({latitude: 44.04444,
+    longitude: 42.86056,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,});
+  const [urlTemplate, setUrlTemplate] = useState("http://c.tile.openstreetmap.org/{z}/{x}/{y}.png")
+  const [loaded, setLoaded] = useState(false);
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+     <MapView
+          style={{width: "100%", height: "100%"}}
+          initialRegion={region}
+          onRegionChange={setRegion}
+          onLayout={()=>{console.log('loaded');setLoaded(true)}}
+      >  
+      {
+
+        loaded ? 
+        <UrlTile
+    /**
+     * The url template of the tile server. The patterns {x} {y} {z} will be replaced at runtime
+     * For example, http://c.tile.openstreetmap.org/{z}/{x}/{y}.png
+     */
+    urlTemplate={urlTemplate}
+    /**
+     * The maximum zoom level for this tile overlay. Corresponds to the maximumZ setting in
+     * MKTileOverlay. iOS only.
+     */
+    maximumZ={19}
+    /**
+     * flipY allows tiles with inverted y coordinates (origin at bottom left of map)
+     * to be used. Its default value is false.
+     */
+    flipY={false}
+  />
+  : <></>
+
+      }
+      
+  </MapView>
+
+
     </View>
   );
 }
